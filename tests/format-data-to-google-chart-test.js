3bb5a4aad304bc6d3fe0d129d5cab19a55c78b9b
Manuel Vilche <manuelvilche@gmail.com>
2020-09-18 11:47:59 -0300

    Initial commit

---
+++ tests/format-data-to-google-chart-test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// const assert = require('assert');
+
+// const sinon = require('sinon');
+
+// const {
+// 	FormatDataToGoogleChart,
+// 	FormatDataToGoogleChartError
+// } = require('../lib');
+
+// describe('FormatDataToGoogleChart', () => {
+
+// 	// your tests here...
+
+// });
