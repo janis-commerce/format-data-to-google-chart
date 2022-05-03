3c6a3c18d013fbef2074e14a02d9148ca65ca246
Alejandro-Gonzalez <alejandro@fizzmod.com>
2022-05-03 19:23:56 -0300

    add barchart formatter

---
+++ tests/sample-data/bar-chart.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = [
+	{
+		id: 1,
+		name: 'First element',
+		quantity: 10,
+		color: 'red',
+		key: 'A1'
+	},
+	{
+		id: 2,
+		name: 'Second element',
+		quantity: 20,
+		color: 'blue',
+		key: 'A2'
+	},
+	{
+		id: 3,
+		name: 'Third element',
+		quantity: 60,
+		color: 'black',
+		key: 'A3'
+	}
+];
---
+++ tests/unit/bar-chart.js
@@ -0,0 +1,322 @@
+'use strict';
+
+const assert = require('assert');
+
+const sampleData = require('../sample-data/bar-chart');
+const { BarChart } = require('../../lib');
+
+describe('Bar Chart', () => {
+
+	it('Should parse the data and options property', () => {
+
+		const barChart = new BarChart({
+			label: {
+				source: 'name'
+			},
+			values: [
+				{ source: 'quantity' }
+			]
+		});
+
+		barChart.setData(sampleData);
+
+		const {
+			data,
+			options
+		} = barChart.parse();
+
+		assert.deepStrictEqual(options, {});
+
+		assert.deepStrictEqual(data, [
+			['name', 'quantity'],
+			['First element', 10],
+			['Second element', 20],
+			['Third element', 60]
+		]);
+	});
+
+	it('Should parse the data and options property with styles and annotation', () => {
+
+		const barChart = new BarChart({
+			label: {
+				source: 'name'
+			},
+			values: [
+				{ source: 'quantity' },
+				{ source: 'color' },
+				{ source: 'key' }
+			]
+		});
+
+		barChart.setData(sampleData);
+
+		const {
+			data,
+			options
+		} = barChart.parse();
+
+		assert.deepStrictEqual(options, {});
+
+		assert.deepStrictEqual(data, [
+			['name', 'quantity', { role: 'style' }, { role: 'annotation' }],
+			['First element', 10, 'red', 'A1'],
+			['Second element', 20, 'blue', 'A2'],
+			['Third element', 60, 'black', 'A3']
+		]);
+	});
+
+	it('Should use label and values titles if present', () => {
+
+		const barChart = new BarChart({
+			label: {
+				source: 'name',
+				title: 'Name'
+			},
+			values: [
+				{ source: 'quantity', title: 'Quantity' },
+				{ source: 'color' },
+				{ source: 'key' }
+			]
+		});
+
+		barChart.setData(sampleData);
+
+		const {
+			data,
+			options
+		} = barChart.parse();
+
+		assert.deepStrictEqual(options, {});
+
+		assert.deepStrictEqual(data, [
+			['Name', 'Quantity', { role: 'style' }, { role: 'annotation' }],
+			['First element', 10, 'red', 'A1'],
+			['Second element', 20, 'blue', 'A2'],
+			['Third element', 60, 'black', 'A3']
+		]);
+	});
+
+	it('Should pass-through pie chart properties', () => {
+
+		const barChart = new BarChart({
+			label: {
+				source: 'name'
+			},
+			values: [
+				{ source: 'quantity' },
+				{ source: 'color' },
+				{ source: 'key' }
+			]
+		}, {
+			title: 'Density of Precious Metals, in g/cm^3',
+			bar: { groupWidth: '95%' },
+			legend: { position: 'none' }
+		});
+
+		barChart.setData(sampleData);
+
+		const {
+			data,
+			options
+		} = barChart.parse();
+
+		assert.deepStrictEqual(options, {
+			title: 'Density of Precious Metals, in g/cm^3',
+			bar: { groupWidth: '95%' },
+			legend: { position: 'none' }
+		});
+
+		assert.deepStrictEqual(data, [
+			['name', 'quantity', { role: 'style' }, { role: 'annotation' }],
+			['First element', 10, 'red', 'A1'],
+			['Second element', 20, 'blue', 'A2'],
+			['Third element', 60, 'black', 'A3']
+		]);
+	});
+
+	it('Should map a value by it\'s given valueMapper', () => {
+
+		const barChart = new BarChart({
+			label: {
+				source: 'name'
+			},
+			values: [
+				{ source: 'quantity', valueMapper: v => v * 10 },
+				{ source: 'color' },
+				{ source: 'key' }
+			]
+		});
+
+		barChart.setData(sampleData);
+
+		const {
+			data,
+			options
+		} = barChart.parse();
+
+		assert.deepStrictEqual(options, {});
+
+		assert.deepStrictEqual(data, [
+			['name', 'quantity', { role: 'style' }, { role: 'annotation' }],
+			['First element', 100, 'red', 'A1'],
+			['Second element', 200, 'blue', 'A2'],
+			['Third element', 600, 'black', 'A3']
+		]);
+	});
+
+	it('Should map a label by it\'s given valueMapper', () => {
+
+		const barChart = new BarChart({
+			label: {
+				source: 'name',
+				valueMapper: v => `(${v})`
+			},
+			values: [
+				{ source: 'quantity' },
+				{ source: 'color' },
+				{ source: 'key' }
+			]
+		});
+
+		barChart.setData(sampleData);
+
+		const {
+			data,
+			options
+		} = barChart.parse();
+
+		assert.deepStrictEqual(options, {});
+
+		assert.deepStrictEqual(data, [
+			['name', 'quantity', { role: 'style' }, { role: 'annotation' }],
+			['(First element)', 10, 'red', 'A1'],
+			['(Second element)', 20, 'blue', 'A2'],
+			['(Third element)', 60, 'black', 'A3']
+		]);
+	});
+
+	it('Should map a title by it\'s given titleMapper', () => {
+
+		const barChart = new BarChart({
+			label: {
+				source: 'name',
+				titleMapper: v => `The ${v}`
+			},
+			values: [
+				{ source: 'quantity' },
+				{ source: 'color' },
+				{ source: 'key' }
+			]
+		});
+
+		barChart.setData(sampleData);
+
+		const {
+			data,
+			options
+		} = barChart.parse();
+
+		assert.deepStrictEqual(options, {});
+
+		assert.deepStrictEqual(data, [
+			['The name', 'quantity', { role: 'style' }, { role: 'annotation' }],
+			['First element', 10, 'red', 'A1'],
+			['Second element', 20, 'blue', 'A2'],
+			['Third element', 60, 'black', 'A3']
+		]);
+	});
+
+	it('Should sort the data by it\'s key', () => {
+
+		const barChart = new BarChart({
+			label: {
+				source: 'name'
+			},
+			values: [
+				{ source: 'quantity' },
+				{ source: 'color' },
+				{ source: 'key' }
+			],
+			sortData: (key1, key2) => key2.localeCompare(key1)
+		});
+
+		barChart.setData(sampleData);
+
+		const {
+			data,
+			options
+		} = barChart.parse();
+
+		assert.deepStrictEqual(options, {});
+
+		assert.deepStrictEqual(data, [
+			['name', 'quantity', { role: 'style' }, { role: 'annotation' }],
+			['Third element', 60, 'black', 'A3'],
+			['Second element', 20, 'blue', 'A2'],
+			['First element', 10, 'red', 'A1']
+		]);
+	});
+
+	it('Should not sort the titles', () => {
+
+		const barChart = new BarChart({
+			label: {
+				source: 'name'
+			},
+			values: [
+				{ source: 'quantity' },
+				{ source: 'color' },
+				{ source: 'key' }
+			],
+			sortData: (key1, key2) => key1.localeCompare(key2)
+		});
+
+		barChart.setData(sampleData);
+
+		const {
+			data,
+			options
+		} = barChart.parse();
+
+		assert.deepStrictEqual(options, {});
+
+		assert.deepStrictEqual(data, [
+			['name', 'quantity', { role: 'style' }, { role: 'annotation' }],
+			['First element', 10, 'red', 'A1'],
+			['Second element', 20, 'blue', 'A2'],
+			['Third element', 60, 'black', 'A3']
+		]);
+	});
+
+	it('Should ignore sort if it\'s not a function', () => {
+
+		const barChart = new BarChart({
+			label: {
+				source: 'name'
+			},
+			values: [
+				{ source: 'quantity' },
+				{ source: 'color' },
+				{ source: 'key' }
+			],
+			sortData: 'someInvalidSort'
+		});
+
+		barChart.setData(sampleData);
+
+		const {
+			data,
+			options
+		} = barChart.parse();
+
+		assert.deepStrictEqual(options, {});
+
+		assert.deepStrictEqual(data, [
+			['name', 'quantity', { role: 'style' }, { role: 'annotation' }],
+			['First element', 10, 'red', 'A1'],
+			['Second element', 20, 'blue', 'A2'],
+			['Third element', 60, 'black', 'A3']
+		]);
+	});
+
+});
